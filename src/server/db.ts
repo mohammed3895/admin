81b15a6fe248e78f57b5afd9b2e1e2cd5deb8a0b
mohammed sameer <85784627+mohammed3895@users.noreply.github.com>
2024-04-24 21:32:24 +0200

    fix linting errors

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "~/env";
-
 const createPrismaClient = () => new PrismaClient();
 
 const globalForPrisma = globalThis as unknown as {
